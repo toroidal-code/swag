8ce19a6ad33712b6cdfb9470397d93b6d07d0536
kristenmills <kristen@kristen-mills.com>
2013-01-22 15:16:57 -0500

    Added main.m

---
+++ main.m
@@ -0,0 +1,24 @@
+//
+//  swag.m
+//  swag
+//
+//  Created by Kristen Mills on 1/22/13.
+//  Copyright (c) 2013 Kristen Mills. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+	while (true) {
+		NSAlert *alert = [NSAlert alertWithMessageText: @"Swag!"
+										 defaultButton: @"Swag"
+									   alternateButton: nil
+										   otherButton: nil
+							 informativeTextWithFormat: @""];
+		[[NSRunningApplication currentApplication] activateWithOptions:NSApplicationActivateIgnoringOtherApps];
+		[alert runModal];
+		[NSThread sleepForTimeInterval:15];
+	}
+	return 0;
+}
